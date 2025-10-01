45f347821f4bdcf947d66b4c6ca36d52b801dbfe
ambersr <amber.schalker@hva.nl>
2025-10-01 20:55:35 +0200

    style: Added styling to newsCard component #30

--- src/lib/components/NewsCard.svelte
+++ src/lib/components/NewsCard.svelte
@@ -18,3 +18,30 @@
         <ButtonLight href="/">Bekijk artikel</ButtonLight>
     </div>
 </article>
+
+<style>
+    article {
+        border: 1px solid #CCCCCC;
+        border-radius: 1em;
+        padding: .7em;
+    }
+
+    article > img:first-of-type {
+        width: 100%;
+        border-radius: .5em;
+        object-fit: cover;
+    }
+
+    section {
+        padding: 1em;
+        gap: 1em;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start
+    }
+
+    h1, h2, h3, h4, h5, h6, p {
+        margin: 0;
+    }
+  
+</style>
