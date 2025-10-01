e7a9cf6edb3d86a175cfb72666bda90c308db302
ambersr <amber.schalker@hva.nl>
2025-10-01 20:44:09 +0200

    feat: Added HTML newsCard component #30

---
+++ src/lib/components/NewsCard.svelte
@@ -0,0 +1,20 @@
+<script>
+    // Import afbeeldingen
+    import fallback from '$lib/assets/news-fallback.jpg';
+    import calendar from '$lib/assets/icon-calendar.svg';
+
+    // Import component
+    import ButtonLight from "$lib/components/ButtonLight.svelte";
+</script>
+
+<!-- News card -->
+<article>
+    <img src="{fallback}" alt="" width="300" height="210">
+
+    <section>
+        <h3>Titel</h3>
+        <p><img src="{calendar}" alt="" width="15" height="15">17 april 2026</p>
+        <p>Description</p>
+        <ButtonLight href="/">Bekijk artikel</ButtonLight>
+    </section>
+</article>
