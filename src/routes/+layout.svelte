bd79cd224b4490d274629eaf81fbb8cb22198574
Nayomekaia <nayomedoelwijt@gmail.com>
2025-10-01 15:16:57 +0200

    imported the 2 fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,20 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
+	//importeren
+  </script>
+  
+  <svelte:head>
+	<!-- Fonts -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://api.fontshare.com/v2/css?f[]=clash-display@400,500,600&display=swap">
+  </svelte:head>
+  <!-- stylesheet -->
+  <link rel="stylesheet" href="stylesheet.css">
+  
+  <main>
+  <slot />
+</main>
+  
 
-	let { children } = $props();
-</script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
-
-{@render children?.()}
