8cb5145f71f8ea0c064f7e0b0ea99f1e973b7509
Nayomekaia <nayomedoelwijt@gmail.com>
2025-10-05 15:34:30 +0200

    added a in stylesheet

--- src/lib/components/ButtonLight.svelte
+++ src/lib/components/ButtonLight.svelte
@@ -9,19 +9,10 @@
 
 <style>
     a {
-        padding: .6em 1.2em;
-        border: 1px solid var(--secondary-text);
-        background-color: #fff;
-        color: #000A1A;
-        border: 1px solid #00408D;
-        border-radius: .5em;
         transition: .2s ease-in-out;
-        text-decoration: none;
     }
 
     a:hover {
-        background-color: #00408D;
-        color: #fff;
         transition: .2s ease-in-out;
     }
 </style>
