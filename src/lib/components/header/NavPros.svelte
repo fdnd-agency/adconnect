7867056d81592da1be6b198b299ace577a9a6243
Nayomekaia <nayomedoelwijt@gmail.com>
2025-10-08 17:03:07 +0200

    html prototype navpro

---
+++ src/lib/components/header/NavPros.svelte
@@ -0,0 +1,26 @@
+<script>
+  import logo from '$lib/assets/logo.svg';
+  import logomobile from '$lib/assets/logomobile.svg';
+</script>
+
+<nav>
+  <img class="logo logo--mobile" src={logomobile} alt="Logo" />
+  <img class="logo logo--desktop" src={logo} alt="Logo" />
+
+  <details class="menu">
+    <summary>
+      <span></span>
+      <span></span>
+      <span></span>
+    </summary>
+    <ul class="panel">
+      <li><a href="/over-ad">Over Associate degree's</a></li>
+      <li><a href="/documenten">Documenten</a></li>
+      <li><a href="/talentaward">TalentAward</a></li>
+      <li><a href="/nieuws">Nieuws</a></li>
+      <li><a href="/ad-dag">Kom naar Ad-dag</a></li>
+      <li><a href="nominaties">Nominanties</a></li>
+      <li><a href="over-ons">over ons</a></li>
+      <li><a href="/contact">contact</a></li>
+    </ul>
+  </details>
