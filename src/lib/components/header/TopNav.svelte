cb80f52c35e4ad627d4b03533bc58a7ceeabcbe6
Nayomekaia <nayomedoelwijt@gmail.com>
2025-10-08 17:05:56 +0200

    html prototype topnav

---
+++ src/lib/components/header/TopNav.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { page } from '$app/stores';
+</script>
+
+<nav>
+  <section class="nav-left">
+    <ul>
+      <li>
+        <a href="/" class="pro {currentPath === '/' ? 'active' : ''}">
+          Professionals
+        </a>
+      </li>
+      <li>
+        <a href="/students" class="stu {currentPath === '/students' ? 'active' : ''}">
+          Studenten
+        </a>
+      </li>
+    </ul>
+  </section>
+
+  <section class="nav-right">
+    <ul>
+      <li><a href="/nominaties">Nominaties</a></li>
+      <li><a href="/over-ons">Over ons</a></li>
+      <li><a href="/contact">Contact</a></li>
+    </ul>
+  </section>
+</nav>
