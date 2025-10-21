f2664c26d583f97b576958e680efc96c7f403f09
ambersr <amber.schalker@hva.nl>
2025-10-21 12:00:44 +0200

    feat: added HTML theme card #91

---
+++ src/lib/components/theme/ThemeCard.svelte
@@ -0,0 +1,20 @@
+<script>
+    // Retrieve themes data
+    export let themes;
+
+    // Import image
+    import dots from '$lib/assets/dots.svg';
+    
+</script>
+
+{#each themes as theme}
+  <article>
+    <div class="wrapper-text">
+        <img src={dots} alt="" width="50" height="50" />
+         <h3 class="truncate single">{theme.title}</h3>
+    </div>
+    <p class="truncate two">{theme.description}</p>
+    <a class="button-outline-blue" href={`over-ad/${theme.slug}`}>Meer informatie</a>
+  </article>
+{/each}
+
