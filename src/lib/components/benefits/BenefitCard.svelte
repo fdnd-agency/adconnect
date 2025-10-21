4cd9f92831d2e8c2016d49272ea74046a11fb034
ambersr <amber.schalker@hva.nl>
2025-10-21 12:51:13 +0200

    style: added css benefit card component #92

--- src/lib/components/benefits/BenefitCard.svelte
+++ src/lib/components/benefits/BenefitCard.svelte
@@ -13,3 +13,22 @@
     <p class="truncate two">{excerpt}</p>
     <a class="button-outline-blue" href="/">Lees meer</a>
 </article>
+
+<style>
+    article {
+        display: flex;
+        flex-direction: column;
+        gap: 1em;
+        padding: 1.5em;
+        border: 1px solid var(--neutral-300);
+        border-radius: 1em;
+    }
+
+    img {
+        width: 1em;
+        background-color: var(--orange-500);
+        padding: 1em;
+        border-radius: .5em;
+        margin: -3em 0 0 0;
+    }
+</style>
