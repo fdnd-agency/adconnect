824458e723d81a604e6fca906388b13c18d1dff7
ambersr <amber.schalker@hva.nl>
2025-10-21 12:48:29 +0200

    feat: added HTML component benefit card #92

---
+++ src/lib/components/benefits/BenefitCard.svelte
@@ -0,0 +1,15 @@
+<script>
+    // Export prop
+    export let title = "";
+    export let excerpt = "";
+
+    // Import afbeeldingen
+    import check from '$lib/assets/icon-check-white.svg';
+</script>
+
+<article>
+    <img src="{check}" alt="">
+    <h3>{title}</h3>
+    <p class="truncate two">{excerpt}</p>
+    <a class="button-outline-blue" href="/">Lees meer</a>
+</article>
