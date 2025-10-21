1fc1057acb7d779d24ef7febe49f2a1cb49b4288
ambersr <amber.schalker@hva.nl>
2025-10-21 12:38:12 +0200

    feat: added HTML ImageText component #93

---
+++ src/lib/components/info/ImageTextSection.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let image;
+    export let dots;
+    export let title;
+    export let text;
+    export let link = "#";
+    export let linkText = "Lees meer";
+    export let imagePosition = "left"; // 'left' of 'right'
+    export let background = "blue";    // 'blue' of 'white'
+</script>
+
+<section
+    class:image-right={imagePosition === 'right'}
+    class:image-left={imagePosition === 'left'}
+    class:bg-blue={background === 'blue'}
+    class:bg-white={background === 'white'}
+    >
+    <div class="wrapper">
+        <img class="visual" src={image} alt="" />
+
+        <div class="content">
+            <img src={dots} alt="" width="80" height="80" />
+            <h2>{title}</h2>
+            <p>{text}</p>
+            <a class="button-outline-white" href={link}>{linkText}</a>
+        </div>
+    </div>
+</section>
