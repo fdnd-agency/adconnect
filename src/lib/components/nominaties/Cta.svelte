f269e6ff7b9c1d755878202fc92cf4855441d873
Nayomekaia <nayomedoelwijt@gmail.com>
2025-10-24 01:32:15 +0200

    HTML Prototype
    #111

---
+++ src/lib/components/nominaties/Cta.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Divider from '$lib/components/divider/Divider.svelte';
+</script>
+
+<Divider />
+<h2> Nomineer nu </h2>
+<p>
+    Lorem ipsum dolor sit amet consectetur. Semper semper neque 
+    diam ac facilisi orci. Amet ultrices et et sagittis. Pharetra 
+    sit elit rhoncus leo nisi facilisis sed purus et. Pellentesque
+     praesent nisl sed egestas sapien. Odio pellentesque morbi purus
+      erat tempor. Non sed tristique malesuada phasellus dolor.
+       Malesuada orci phasellus a amet lectus nisi massa massa.
+</p>
+<a href="/nominaties" class="button-outline-primary">Nomineer</a>
+
+<Divider />
