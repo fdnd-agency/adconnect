0ca11822aef80882b827b9704b287c67ae9abc88
Nayomekaia <nayomedoelwijt@gmail.com>
2025-10-24 01:26:38 +0200

    HTML Prototype
    #106

---
+++ src/lib/components/talentaward/PreviousWinners.svelte
@@ -0,0 +1,13 @@
+<script>
+  import DividerText from '$lib/components/divider/DividerText.svelte';
+</script>
+
+<DividerText text="Voorgaande talent award winaars" />
+
+here is going to be a list of previous winners dynamic
+
+<a href="/talentaward" class="button-outline-primary">Bekijk Talents</a>
+
+
+
+
