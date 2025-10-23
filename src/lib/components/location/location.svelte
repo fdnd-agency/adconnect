19f2d761ea27a1fff114dd523f08d68e437014d7
Nayomekaia <nayomedoelwijt@gmail.com>
2025-10-23 23:30:11 +0200

    HTML Prototype
    #99

--- src/lib/components/location/location.svelte
+++ src/lib/components/location/location.svelte
@@ -1,3 +1,15 @@
 <script>
     import DividerText from "../divider/DividerText.svelte";
 </script>
+
+
+<DividerText text="Locatie Ad Dag " />
+<p> Lorem ipsum dolor sit amet consectetur. Ultrices at quis 
+    pellentesque at eget ut suspendisse. 
+    Rhoncus purus ultrices quis eu lectus interdum 
+    egestas iaculis. Pellentesque elementum urna.
+</p>
+<p> Locatie : Arboretum West 98, 1325 WB Almere </p>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d623779.8517481206!2d4.826726400000002!3d52.35881345000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sAeres%20Hogeschool!5e0!3m2!1sen!2snl!4v1761253603041!5m2!1sen!2snl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
