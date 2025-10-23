fbea0f6b1f5ecbe5563c1c362273ee42440ed8f2
ambersr <amber.schalker@hva.nl>
2025-10-23 12:53:13 +0200

    feat: retrieve theme data from Directus API #116

---
+++ src/routes/over-ad/[slug]/+page.server.js
@@ -0,0 +1,19 @@
+export async function load({ params }) {
+    const { slug } = params;
+
+    // Data from Directus API
+    const baseUrl = "https://fdnd-agency.directus.app/items/";
+    const themeEndpoint = "adconnect_themes";
+    const fields = "&fields=title,description,hero,slug,body";
+    const filter = `?filter[slug][_eq]=${slug}`;
+
+    // Convert data to json
+    const themeResponse = await fetch(`${baseUrl}${themeEndpoint}${filter}${fields}`);
+    const themeData = await themeResponse.json();
+
+    console.log(themeData)
+
+    return {
+        theme: themeData.data,
+    };
+}
---
+++ src/routes/over-ad/[slug]/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import NavPros from '$lib/components/header/NavPros.svelte';
+    import Hero from "$lib/components/hero/Hero.svelte";
+
+    import placeholder from "$lib/assets/placeholder-hero.png";
+
+    let { data } = $props(); 
+
+    const theme = data.theme[0];
+</script>
