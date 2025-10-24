3cf730b17939b327786e67ce1a29badd177dcd2c
Nayomekaia <nayomedoelwijt@gmail.com>
2025-10-25 00:58:04 +0200

    mobile version
    #106

--- src/lib/components/talentaward/PreviousWinners.svelte
+++ src/lib/components/talentaward/PreviousWinners.svelte
@@ -23,6 +23,62 @@
   </section>
 </section>
 
+<style>
+  .previous-winners {
+    background-color: var(--blue-100);
+    padding: 2rem;
+    border-radius: 15px;
+  }
 
+  section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1.5rem;
+    margin-bottom: 2rem;
+    margin-top: 1rem;
+  }
 
+  .slider {
+    overflow: hidden;
+    width: 90%;
+    width: 100vw;
+    max-width: 1400px;
+  }
 
+  ul {
+    display: flex;
+    animation: scroll 10s linear infinite;
+  }
+
+  li {
+    flex: 0 0 20%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  img {
+    max-height: 150px;
+    filter: grayscale(100%) brightness(0.9);
+    opacity: 0.8;
+    width: auto;
+    border-radius: 15px;
+  }
+
+  img:hover {
+    filter: grayscale(0%) brightness(1);
+    opacity: 1;
+  }
+
+  @keyframes scroll {
+    0% {
+      transform: translateX(0);
+    }
+
+    100% {
+      transform: translateX(-25%);
+    }
+  }
+
+</style>
