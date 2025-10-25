faf77800182ed6de71bd56e51b7510a397c38365
Nayomekaia <nayomedoelwijt@gmail.com>
2025-10-26 01:16:34 +0200

    Update Cta.svelte

--- src/lib/components/nominaties/Cta.svelte
+++ src/lib/components/nominaties/Cta.svelte
@@ -1,22 +1,10 @@
 <script>
-    import Divider from '$lib/components/divider/Divider.svelte';
     import Divider from "$lib/components/divider/Divider.svelte";
     import darkcircle from "$lib/assets/dark-circle.svg";
 </script>
 
 <Divider />
-<h2> Nomineer nu </h2>
-<p>
-    Lorem ipsum dolor sit amet consectetur. Semper semper neque 
-    diam ac facilisi orci. Amet ultrices et et sagittis. Pharetra 
-    sit elit rhoncus leo nisi facilisis sed purus et. Pellentesque
-     praesent nisl sed egestas sapien. Odio pellentesque morbi purus
-      erat tempor. Non sed tristique malesuada phasellus dolor.
-       Malesuada orci phasellus a amet lectus nisi massa massa.
-</p>
-<a href="/nominaties" class="button-outline-primary">Nomineer</a>
 
-<Divider />
 <section>
     <article>
         <img class="circle-left" src={darkcircle} alt="" />
