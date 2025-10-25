edc37595ea4b4810a07350da011c4d2ee8b0044c
Nayomekaia <nayomedoelwijt@gmail.com>
2025-10-26 00:16:33 +0200

    Mobile version
    #108

--- src/lib/components/ThisYearsWinner.svelte
+++ src/lib/components/ThisYearsWinner.svelte
@@ -25,3 +25,49 @@
 
   </section>
 </section>
+
+<style>
+.winner {
+  text-align: left;
+  margin: 0 auto;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  background-color: var(--blue-100);
+
+h2 {
+  text-align: center;
+  margin-bottom: 1em;
+  margin-top: 1em;
+
+}
+
+p {
+        max-width: 500px;
+        margin: 0 auto;
+        text-align: left;
+    }
+}
+
+.winner img {
+  display: block;
+  align-self: center;
+  max-width: 100%;
+  margin: 3em auto 0;
+  height: auto;
+  border-radius: 15px;
+}
+
+.winner-detail {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 0.5rem;
+
+
+   a { 
+   margin-bottom: 2em;
+
+  }
+}
+</style>
