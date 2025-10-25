4120dd6b5772fadd0034296b0011d1f684c64442
ambersr <amber.schalker@hva.nl>
2025-10-25 14:47:56 +0200

    style: mobile first DocumentCard component #123

--- src/lib/components/documents/DocumentCard.svelte
+++ src/lib/components/documents/DocumentCard.svelte
@@ -21,3 +21,64 @@
     <a class="button-outline-blue" href="/publicaties/{document.slug}">Meer informatie</a>
 </article>
 
+<style>
+    article {
+        display: flex;
+        flex-direction: column;
+        gap: 1.5em;
+        border: 1px solid #CCCCCC;
+        border-radius: 1em;
+        padding: 2em;
+        transition: .2s ease-in-out;
+
+        &:hover {
+            border: 1px solid #00408D;
+            box-shadow: 0 3px 10px rgba(141, 141, 141, 0.2);
+            translate: 0 -1%;
+            transition: .2s ease-in-out;
+        }
+    }
+
+    .card-info {
+        gap: 1em;
+        display: flex;
+        flex-direction: row;
+
+        span {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            gap: .5em;
+        }
+    }
+
+    h3, p {
+        margin: 0;
+        width: 100%;
+    }
+
+    h3 {
+        font-size: 25px;
+        height: 2.5em;
+    }
+
+    /* Truncate words */
+    .truncate {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box; 
+        -webkit-box-orient: vertical;
+        }
+
+    /* Truncate one sentence */
+    .truncate.single {
+        white-space: nowrap;
+        -webkit-line-clamp: 1;
+    }
+
+    /* Truncate two sentences */
+    .truncate.two {
+        -webkit-line-clamp: 2;
+    }
+  
+</style>
