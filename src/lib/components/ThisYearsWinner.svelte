c3bed12930158408fc01b09d7358db959de8079e
Nayomekaia <nayomedoelwijt@gmail.com>
2025-10-26 00:16:13 +0200

    updated html prototype
    #108

---
+++ src/lib/components/ThisYearsWinner.svelte
@@ -0,0 +1,27 @@
+
+<script>
+  import Winner from '$lib/assets/winner.png';
+</script>
+
+<section class="winner">
+    <section class='info'>
+  <h2> Dit Jaars Winaar</h2>
+    <p> Lorem ipsum dolor sit amet consectetur. Semper semper neque diam ac facilisi orci.
+        Amet ultrices et et sagittis. Pharetra sit elit rhoncus leo nisi facilisis sed purus et.
+        Pellentesque praesent nisl sed egestas sapien. Odio pellentesque morbi purus erat tempor.
+        Non sed tristique malesuada phasellus dolor. Malesuada orci phasellus a amet lectus nisi 
+        massa massa.
+    </p>
+    
+  </section>
+  <section class='visual'>
+    
+    <img src={Winner} alt="Ad Talent Award Winner" width="290" height="300" />
+    <section class='winner-detail'>
+      <h5> Naam Winaar </h5>
+      <a href="#school" style='color: var(--primary-orange);'> Link school</a>
+      <a href="#learn-more" class="button-outline-white" id="learn-more">Lees meer</a>
+   </section>
+
+  </section>
+</section>
