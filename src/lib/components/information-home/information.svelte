26d6a7664ac33fa81e1cddc3ffd67a48d155978d
ArmanVD <Arman22.av@gmail.com>
2025-10-26 19:44:01 +0100

    #52 finished component information

---
+++ src/lib/components/information-home/information.svelte
@@ -0,0 +1,70 @@
+<script>
+  import dots from "$lib/assets/dots.svg";
+  import imageSrc from "$lib/assets/doorstroom-ad-dag.jpg";
+
+  export let title = "";
+  export let description = "";
+  export let buttonText = "";
+  export let buttonLink = "#";
+  export let imageAlt = "";
+</script>
+
+<section class="information">
+  <div class="information-content">
+    <img src={dots} alt="" class="information-dots" />
+    <h2 class="information-title">{title}</h2>
+    <p class="information-description">{description}</p>
+
+    <a href={buttonLink} class="button-outline-white">
+      {buttonText}
+    </a>
+  </div>
+
+  <div class="information-image">
+    <img src={imageSrc} alt={imageAlt} />
+  </div>
+</section>
+
+<style>
+  .information {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+    padding: 4rem 2rem;
+    background: var(--background);
+    margin-bottom: -7.5rem;
+  }
+
+  .information-content {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    text-align: left;
+    width: 75%;
+    gap: 0.75em;
+  }
+
+  .information-image {
+    width: 80%;
+  }
+
+  .information-image img {
+    width: 100%;
+    border-radius: 33px 33px 0 0;
+    object-fit: cover;
+  }
+
+  @media (min-width: 768px) {
+    .information {
+      margin-bottom: -10rem;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    .information {
+      margin-bottom: -15rem;
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
   import NewsCardSection from "$lib/components/NewsCardSection.svelte";
-  import NavPros from '$lib/components/header/NavPros.svelte';
+  import NavPros from "$lib/components/header/NavPros.svelte";
   import Hero from "$lib/components/hero/Hero.svelte";
   import placeholder from "$lib/assets/placeholder-hero.png";
+  import Information from "$lib/components/information-home/information.svelte";
 
-  let { data } = $props(); 
+  let { data } = $props();
   const news = data.news;
 </script>
 
@@ -14,10 +15,7 @@
 
 <NavPros />
 
-
-<Hero
-  title="Overlegplatform Associate Degrees"
-  description="Het overlegplatform voor Associate Degree opleidingen in Nederland.">
+<Hero title="Overlegplatform Associate Degrees" description="Het overlegplatform voor Associate Degree opleidingen in Nederland.">
   <a slot="primary" href="/dev/hero" class="button-outline-white">Lorum Ipsum →</a>
   <a slot="secondary" href="/dev/hero" class="button-outline-blue">Lorum Action →</a>
   <img class="hero-image" src={placeholder} alt="" />
@@ -25,28 +23,34 @@
 
 <NewsCardSection news={data.news.slice(0, 3)} />
 
+<Information
+  title="Wat zijn Associate degrees en hoe sluit het aan bij jou wensen?"
+  description="Lorem ipsum dolor sit amet consectetur. Id dolor elementum ultrices feugiat aliquam ornare phasellus fames. Egestas libero sit elementum felis tellus fames imperdiet consectetur. Mauris tincidunt nibh ac at. Suscipit eu urna non est. Etiam mi auctor sapien diam tortor. Pharetra quisque ipsum velit tortor velit. Risus elementum morbi commodo est. Risus sed purus dui dolor odio lobortis nulla. Ac ultricies adipiscing felis ultricies viverra aliquam molestie. Tellus orci volutpat sit id quam varius nam nunc. Facilisis dui risus duis id erat interdum consectetur risus pharetra. Posuere purus ultricies auctor."
+  buttonText="Lorem ipsum dolor"
+  buttonLink="#"
+  imageAlt="Studenten ontvangen award"
+/>
+
 <style>
-.hero-image {
-  display: block;
-  width: 100%;
-  max-height: 416px;
-  object-fit: cover;
-  border-radius: 30px 30px 0 0;
-}
-
-@media (min-width: 768px) {
   .hero-image {
-    border-radius: 24px;
-    max-width: 604px;
-    margin-left: auto; 
+    display: block;
+    width: 100%;
+    max-height: 416px;
+    object-fit: cover;
+    border-radius: 30px 30px 0 0;
   }
-}
 
-@media (min-width: 1024px) {
-  .hero-image {
-    max-width: 640px;
+  @media (min-width: 768px) {
+    .hero-image {
+      border-radius: 24px;
+      max-width: 604px;
+      margin-left: auto;
+    }
   }
-}
 
+  @media (min-width: 1024px) {
+    .hero-image {
+      max-width: 640px;
+    }
+  }
 </style>
-
