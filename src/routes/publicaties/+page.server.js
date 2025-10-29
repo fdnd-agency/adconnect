0ab8e6c10da6e245bbe2264ad621210fbb8b3077
ambersr <amber.schalker@hva.nl>
2025-10-29 10:16:43 +0100

    refactor: document server js refactor #130

--- src/routes/publicaties/+page.server.js
+++ src/routes/publicaties/+page.server.js
@@ -1,17 +1,19 @@
 export async function load({ url }) {
+    
+    // Data from Directus API
     const baseUrl = "https://fdnd-agency.directus.app/items/";
     const documentsEndpoint = "adconnect_documents";
     const fields = "?fields=title,id,description,slug,hero_image,source_file,category.*,date";
 
-    // get the category from string
+    // Get the category from string
     const category = url.searchParams.get('category');
 
     let filter;
 
     if (category) {
-        // check if the category isn't 'alle-publicaties'
+        // Check if the category isn't 'alle-publicaties'
         if (category === 'alle-publicaties' || category === '') {
-            // no filter added
+            // No filter added
             filter = '';
         } else {
             // Choose category, then add parameter 'category=..' in url
@@ -23,12 +25,12 @@ export async function load({ url }) {
         filter = '';
     }
 
-    // Retrieve data
+    // Convert document data (with filter) to json
     const filterUrl = baseUrl + documentsEndpoint + fields + filter;
-
     const documentsResponse = await fetch(filterUrl);
     const documentsData = await documentsResponse.json();
 
+    // Convert category data to json
     const categoriesResponse = await fetch(baseUrl + "adconnect_categories");
     const categoriesData = await categoriesResponse.json();
 
