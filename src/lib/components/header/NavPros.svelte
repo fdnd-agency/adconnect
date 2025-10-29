c31eb1934595296d683f40bf8fe49ed888f31419
ambersr <amber.schalker@hva.nl>
2025-10-29 10:08:19 +0100

    fix: link in header talent award #130

--- src/lib/components/header/NavPros.svelte
+++ src/lib/components/header/NavPros.svelte
@@ -15,7 +15,7 @@
     <ul class="panel">
       <li><a href="/over-ad">Over Ad's</a></li>
       <li><a href="/publicaties">Publicaties</a></li>
-      <li><a href="/talentaward">TalentAward</a></li>
+      <li><a href="/talent-award">Talent Award</a></li>
       <li><a href="/nieuws">Nieuws</a></li>
       <li><a href="/ad-dag">Kom naar Ad-dag</a></li>
       <li><a href="/talent-award/nominaties">Nominanties</a></li>
@@ -27,7 +27,7 @@
   <ul class="desktop-nav">
     <li><a class='menu-button' href="/over-ad">Over Ad's</a></li>
     <li><a class='menu-button' href="/publicaties">Publicaties</a></li>
-    <li><a class='menu-button' href="/talentaward">TalentAward</a></li>
+    <li><a class='menu-button' href="/talent-award">Talent Award</a></li>
     <li><a class='menu-button' href="/nieuws">Nieuws</a></li>
     <li><a class='button-outline-white' href="/ad-dag">Kom naar Ad-dag</a></li>
   </ul>
