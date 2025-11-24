97e23998da07b2a985e4e5380152c2dbf332cc15
ambersr <amber.schalker@hva.nl>
2025-11-24 16:16:43 +0100

    fix: loading lazy for image in ImageText component #173

--- src/lib/components/info/ImageTextSection.svelte
+++ src/lib/components/info/ImageTextSection.svelte
@@ -20,7 +20,7 @@
     <div class="wrapper">
       <div class="img-wrapper">
         <img class="circle" src="{lightcircle}" alt="">
-        <img class="visual" src={image} alt="{alt}">
+        <img class="visual" src={image} alt="{alt}" loading="lazy">
       </div>
 
         <div class="content">
