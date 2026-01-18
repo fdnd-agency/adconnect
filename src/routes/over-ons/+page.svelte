7b57edd022d1e50438b46f7c41c6e40d16057d67
ArmanVD <Arman22.av@gmail.com>
2026-01-19 00:32:42 +0100

    #217 added text

--- src/routes/over-ons/+page.svelte
+++ src/routes/over-ons/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   // Import images
   import { doorstroom, overleggen, awards, dots, placeholder, overad } from "$lib";
+  import logomobile from "$lib/assets/logomobile.svg";
 
   // Import components
   import { BenefitsCard, NavPros, ThemeCard, ImageTextSection, Divider, DividerText, LogoSection, Hero } from "$lib";
@@ -23,3 +24,14 @@
 >
   <img class="hero-image" src={overad} alt="" fetchpriority="high" />
 </Hero>
+
+<section class="intro">
+  <img class="logo" src={logomobile} alt="Ad Talent Award Logo" width="50" height="50" />
+  <h2>Waarom AdConnect?</h2>
+  <p>
+    Het platform stimuleert naamsbekendheid en kennisdeling over de Associate degree. Het platform doet dit met en vanuit Vereniging Hogescholen en
+    zet zich in om samenwerking te bevorderen tussen onderwijs- en werkveldpartners en de overheid om de Ad te positioneren in Nederland. Ook
+    ondersteunt het platform de Landelijke Ad-overleggen (LAdO’s) waarin samengewerkt wordt aan de gezamenlijke profilering van de vergelijkbare
+    Ad-opleidingen.
+  </p>
+</section>
