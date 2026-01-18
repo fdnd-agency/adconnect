d251b539aeb9564add936a0cb444dca3c8606441
ArmanVD <Arman22.av@gmail.com>
2026-01-19 00:27:18 +0100

    #217 added menu and fixed hero

--- src/routes/over-ons/+page.svelte
+++ src/routes/over-ons/+page.svelte
@@ -1,5 +1,23 @@
 <script>
-  import { Hero } from "$lib";
+  // Import images
+  import { doorstroom, overleggen, awards, dots, placeholder, overad } from "$lib";
+
+  // Import components
+  import { BenefitsCard, NavPros, ThemeCard, ImageTextSection, Divider, DividerText, LogoSection, Hero } from "$lib";
+
+  let { data } = $props();
+
+  const themes = data.themes;
+  const cooperation = data.cooperation;
 </script>
 
-<Hero />
+<svelte:head>
+  <title>Over ons | Overlegplatform Associate Degrees</title>
+</svelte:head>
+
+<NavPros />
+
+<Hero
+  title="Over ons"
+  description="Het Overlegplatform Associate degrees is opgericht in 2019 en biedt een platform voor iedereen die geïnteresseerd is in Ad-onderwijs. Alle bekostigde hogescholen die Ad-opleidingen aanbieden of dit willen gaan doen, kunnen deelnemen aan het platform."
+/>
