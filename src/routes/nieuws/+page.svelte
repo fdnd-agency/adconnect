2e2135f915742885665ef691a4f21d615ce51951
Nayomekaia <nayomedoelwijt@gmail.com>
2026-01-21 14:40:58 +0100

    imported componets, img and data

--- src/routes/nieuws/+page.svelte
+++ src/routes/nieuws/+page.svelte
@@ -1,5 +1,17 @@
 <script>
     import { Development } from '$lib'
+    import { Nieuwshero } from "$lib";
+
+    /* Import images */
+    import { dots } from "$lib";
+    import { calendar } from "$lib";
+
+    /* Import components */
+    import { NavPros, Hero } from "$lib";
+    import { formatDateNL } from "$lib/molecules/date";
+
+    // Haal data op uit page.server.js via props
+    let { data } = $props();
 </script>
 
 <Development />
