8556c7093244931ed26716390cd9ad991ff03e53
Nayomekaia <nayomedoelwijt@gmail.com>
2026-01-21 14:38:03 +0100

    imported components images and linked data

---
+++ src/routes/nieuws/[uuid]/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { goto } from "$app/navigation";
+	import { page } from "$app/stores";
+
+	// Import images
+	import { Nieuwshero } from "$lib";
+
+	// Import components
+	import { NavPros, Hero } from "$lib";
+
+	// Haal data op uit page.server.js via props
+	let { data } = $props();
+</script>
+		/>
+	{/each}
+</section>
+
+<style>
