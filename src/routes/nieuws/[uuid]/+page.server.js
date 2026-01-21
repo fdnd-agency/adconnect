4e24feb380d7308cba09856e61f83fd9a7f807a2
Nayomekaia <nayomedoelwijt@gmail.com>
2026-01-21 14:40:15 +0100

    made dynamic route to detailpage
    made dynamic route to detail page with uuid

---
+++ src/routes/nieuws/[uuid]/+page.server.js
@@ -0,0 +1,18 @@
+import { error } from '@sveltejs/kit';
+
+export async function load({ params, fetch }) {
+	const res = await fetch(
+		`https://fdnd-agency.directus.app/items/adconnect_news?filter[uuid][_eq]=${params.uuid}`
+	);
+
+	const json = await res.json();
+
+	if (!json.data || json.data.length === 0) {
+		throw error(404, 'Nieuwsartikel niet gevonden');
+	}
+
+	return {
+		news: json.data
+	};
+}
+
