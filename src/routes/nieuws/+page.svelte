447a8c32cd5d35272c95fff21a7d4803da5622a2
Nayomekaia <nayomedoelwijt@gmail.com>
2026-01-21 14:41:21 +0100

    hero

--- src/routes/nieuws/+page.svelte
+++ src/routes/nieuws/+page.svelte
@@ -14,4 +14,25 @@
     let { data } = $props();
 </script>
 
-<Development />
+<Development /><svelte:head>
+    <title>Nieuws | Overlegplatform Associate Degrees</title>
+</svelte:head>
+
+<NavPros />
+
+<Hero
+    title="Nieuws"
+    description="Op deze pagina vind je updates en korte verslagen van georganiseerde evenementen."
+>
+    <img class="hero-image" src={Nieuwshero} alt="" fetchpriority="high" />
+</Hero>
+
+<section class="news">
+    <section class="intro">
+        <img class="dots" src={dots} alt="" width="80px" height="60px" />
+        <h1>Bekijk ons laatste nieuws!</h1>
+        <p>
+            Blijf op de hoogte van alles rond Associate degree-onderwijs in
+            Nederland.
+        </p>
+    </section>
