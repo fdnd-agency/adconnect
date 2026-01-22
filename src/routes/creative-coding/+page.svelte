32ad9b22d328b10f235f973128d30cb0a8a95759
Nayomekaia <nayomedoelwijt@gmail.com>
2026-01-23 00:42:38 +0100

    Html prototype
    made a html prototype with a smart use of classes  #189

--- src/routes/creative-coding/+page.svelte
+++ src/routes/creative-coding/+page.svelte
@@ -8,3 +8,54 @@
 
 <NavPros />
 
+<section class="body">
+    <section class="dream-light"></section>
+
+    <section class="gallery">
+        <section
+        class="img a large"
+        style="background-image:url('https://www.deassociatedegree.nl/wp-content/uploads/2025/01/Stan-van-Hulten-Franken.png')"
+    ></section>
+    
+    <section
+        class="img b small"
+        style="background-image:url('https://www.deassociatedegree.nl/wp-content/uploads/2025/01/Sissi-van-Galen.png')"
+    ></section>
+    
+    <section
+        class="img c tall"
+        style="background-image:url('https://www.deassociatedegree.nl/wp-content/uploads/2025/01/Natasja-Meeuwissen.png')"
+    ></section>
+    
+    <section
+        class="img d medium"
+        style="background-image:url('https://www.deassociatedegree.nl/wp-content/uploads/2025/01/Yvana-van-de-Pavert.png')"
+    ></section>
+    
+    <section
+        class="img e large"
+        style="background-image:url('https://www.deassociatedegree.nl/wp-content/uploads/2025/01/Marin-Kalkhoven.png')"
+    ></section>
+    
+    <section
+        class="img f small"
+        style="background-image:url('https://www.deassociatedegree.nl/wp-content/uploads/2025/01/Jerom-Oude-Aarninkhof.png')"
+    ></section>
+    
+    <section
+        class="img g tall"
+        style="background-image:url('https://www.deassociatedegree.nl/wp-content/uploads/2025/01/Danique-van-Houte.png')"
+    ></section>
+    
+    <section
+        class="img h medium"
+        style="background-image:url('https://www.deassociatedegree.nl/wp-content/uploads/2025/01/Claudia-Scholtens.png')"
+    ></section>
+    
+    <section
+        class="img i large"
+        style="background-image:url('https://www.deassociatedegree.nl/wp-content/uploads/2025/01/Gert-Jan-Hoogstra.png')"
+    ></section>
+    
+    </section>
+</section>
