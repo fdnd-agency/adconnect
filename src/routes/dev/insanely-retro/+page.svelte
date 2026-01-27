39bfd8d21f52cfa06e4c0d84529c57a3e37db5c5
ArmanVD <Arman22.av@gmail.com>
2026-01-27 21:22:12 +0100

    #196 added title

--- src/routes/dev/insanely-retro/+page.svelte
+++ src/routes/dev/insanely-retro/+page.svelte
@@ -83,6 +83,10 @@
   });
 </script>
 
+<svelte:head>
+  <title>Retro Art List</title>
+</svelte:head>
+
 <div class="images">
   <img class="Group2" src="/src/lib/assets/Group 2.svg" alt="" />
   <img class="Group3" src="/src/lib/assets/Group 3.svg" alt="" />
