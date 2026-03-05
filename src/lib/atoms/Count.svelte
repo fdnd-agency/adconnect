281125d87a2f1e98ac9c10450a4c8c646fb444c9
Alex <alex.louwe@hva.nl>
2026-03-05 10:36:32 +0100

    refactor: Splits the documentspage into components - Adds AdminContentList component. - Adds AdminHeader component. - Adds AdminToolbar component. - Adds Count component. Issue #282

---
+++ src/lib/atoms/Count.svelte
@@ -0,0 +1,13 @@
+<script>
+	const { count } = $props()
+</script>
+
+<p class="count">Aantal: {count}</p>
+
+<style>
+	.count {
+		font-family: var(--font-body);
+		font-weight: var(--weight-medium);
+		margin-bottom: 1em;
+	}
+</style>
