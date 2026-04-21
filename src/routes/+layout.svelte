05558f272ba14a247b83644817e4bfe152cc5138
Sidopjescherm <hermussidney@gmail.com>
2026-04-21 11:11:21 +0200

    fix: #502 reset to normal
    The first solution didn't work so I will try a different method

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,12 @@
 <script>
   import favicon from '$lib/assets/favicon-adc.svg'
-
+  
   const { children } = $props()
 </script>
 
 <svelte:head>
   <link rel="icon" href={favicon} />
-  <link 
-    rel="preconnect"
-    as="style" 
-    href="/stylesheet.css" 
-  />
+  <link rel="stylesheet" href="/stylesheet.css"/>
 </svelte:head>
 
 {@render children?.()}
