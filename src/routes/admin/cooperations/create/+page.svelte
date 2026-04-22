33da7037b285cc192584561147124f0cc4ac5477
geemenw <wesley.van.geemen@hva.nl>
2026-04-22 11:42:59 +0200

    refactor:#466-Refactor create form-#548

---
+++ src/routes/admin/cooperations/create/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { DIRECTUS_URL } from '$lib/constants.js'
+	import AdminHeader from '$lib/organisms/AdminHeader.svelte'
+	import CooperationForm from '$lib/organisms/forms/CooperationForm.svelte'
+
+	const { form } = $props()
+	const directusBase = `${DIRECTUS_URL}/admin/content`
+</script>
+
+<svelte:head>
+	<title>Samenwerking toevoegen | ADConnect Admin</title>
+</svelte:head>
+
+<AdminHeader
+	title="Samenwerkingen"
+	{directusBase}
+	contentType="adconnect_cooperations"
+	breadcrumb="Samenwerkingen › Formulier"
+	addHref="/admin/cooperations/create"
+/>
+
+<CooperationForm
+	{form}
+	showPublishButton={true}
+	resetOnSuccess={true}
+	requireLogo={true}
+/>
