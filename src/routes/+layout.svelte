9238c64399ff854414449020933137cb389cf504
Sidopjescherm <hermussidney@gmail.com>
2026-04-22 11:13:50 +0200

    fix: #515 Loading the fonts in
    With the help of @ju5tu5 I actually just wanted to load in the font's instead of the whole CSS document. I will push this code and test in PR #545 if the render-blocking has decreased in size.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,16 @@
 <svelte:head>
   <link rel="icon" href={favicon} />
   <link rel="stylesheet" href="/stylesheet.css"/>
+
+  <!-- Fonts being loaded in -->
+  <link rel="preload" as="font" href="/fonts/Archivo.woff2" type="font/woff2" crossorigin="anonymous"/>
+  <link rel="preload" as="font" href="/fonts/Archivo.woff" type="font/woff" crossorigin="anonymous"/>
+
+  <link rel="preload" as="font" href="/fonts/Archivo Italic.woff2" type="font/woff2" crossorigin="anonymous"/>
+  <link rel="preload" as="font" href="/fonts/Archivo Italic.woff" type="font/woff" crossorigin="anonymous"/>
+
+  <link rel="preload" as="font" href="/fonts/Clash Display Variable.woff2" type="font/woff2" crossorigin="anonymous"/>
+  <link rel="preload" as="font" href="/fonts/Clash Display Variable.woff" type="font/woff" crossorigin="anonymous"/>
 </svelte:head>
 
 {@render children?.()}
