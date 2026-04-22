b9b5da5bfc7787f0806bf7cfb0d03998ebaa9400
Sidopjescherm <hermussidney@gmail.com>
2026-04-22 12:17:59 +0200

    test: #502 test if the background changes (NavPros)
    Seeing if the test for the Navpros background actually works

--- src/lib/organisms/NavPros.svelte
+++ src/lib/organisms/NavPros.svelte
@@ -50,7 +50,10 @@
     align-items: center;
     justify-content: space-between;
     width: 100%;
-    background: var(--background);
+    background: var(--background-color);
+    @media (prefers-color-scheme: dark) {
+      background: var(--blue-800);
+    }
     z-index: 99;
     box-sizing: border-box;
 
