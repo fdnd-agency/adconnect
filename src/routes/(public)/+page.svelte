eb7ecda9a64a2cc2cdaf81614e5e272ccc03e17b
vsheo <vireshsheoratan@gmail.com>
2026-05-21 11:36:08 +0200

    feat: checked icon component #589

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -5,7 +5,7 @@
 	import { MultipleFaq, SingleFaq, DividerText, Divider, LogoSection, Hero, NewsCardSection, Information, InformationCards, FeatureSplit, Link } from '$lib'
 
 	// Import images
-	import { zaal, IconDots } from '$lib'
+	import { zaal, IconChecked } from '$lib'
 
 	const { data } = $props()
 	const { news } = data
@@ -40,7 +40,7 @@
 	/>
 </Hero>
 
-<IconDots variant="heading-three"/>
+<IconChecked />
 
 <NewsCardSection news={data.news.slice(0, 3)} />
 
