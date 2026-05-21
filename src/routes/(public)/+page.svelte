d2e1048bc5938b4595e39007aea002ab4e99a784
vsheo <vireshsheoratan@gmail.com>
2026-05-21 11:26:58 +0200

    feat: dots component #589
    dots component was being used in 2 different sizes, now you can request the variant you need via props

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -5,7 +5,7 @@
 	import { MultipleFaq, SingleFaq, DividerText, Divider, LogoSection, Hero, NewsCardSection, Information, InformationCards, FeatureSplit, Link } from '$lib'
 
 	// Import images
-	import { zaal, IconChevronDown } from '$lib'
+	import { zaal, IconDots } from '$lib'
 
 	const { data } = $props()
 	const { news } = data
@@ -40,8 +40,7 @@
 	/>
 </Hero>
 
-<IconChevronDown />
-
+<IconDots variant="heading-three"/>
 
 <NewsCardSection news={data.news.slice(0, 3)} />
 
