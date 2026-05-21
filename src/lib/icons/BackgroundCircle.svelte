0e1c65bd05494b9d76950991461376480405ff9e
vsheo <vireshsheoratan@gmail.com>
2026-05-21 13:01:34 +0200

    feat: background circle svg #589
    this was duplicate in the assets: dark-circle and light-circle. but the svg code and color was the exact same

---
+++ src/lib/icons/BackgroundCircle.svelte
@@ -0,0 +1,20 @@
+<svg
+	width="var(--_icon-width)"
+	height="var(--_icon-height)"
+	viewBox="0 0 469 469"
+	fill="none"
+>
+	<path
+		d="M437.388 117.684C502.016 229.624 463.663 372.76 351.724 437.388C239.784 502.016 96.648 463.663 32.0198 351.724C-32.6085 239.784 5.7448 96.648 117.684 32.0198C229.624 -32.6084 372.76 5.74482 437.388 117.684Z"
+		fill="var(--_color-fill)"
+		fill-opacity="0.15"
+	/>
+</svg>
+
+<style>
+	svg {
+		--_color-fill: #97AED5;
+		--_icon-width: 469;
+		--_icon-height: 469;
+	}
+</style>
