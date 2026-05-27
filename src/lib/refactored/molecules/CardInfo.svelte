1bece68cebf86367a0002994f94d33dc88b7a542
vsheo <vireshsheoratan@gmail.com>
2026-05-27 11:19:37 +0200

    refactor: info card #640
    refactored classnames and better nesting

--- src/lib/refactored/molecules/CardInfo.svelte
+++ src/lib/refactored/molecules/CardInfo.svelte
@@ -5,22 +5,22 @@
 </script>
 
 <article class="info-card">
-	<div class="heading">
-		<h3>{title}</h3>
+	<div class="info-card__heading">
+		<h3 class="info-card__title">{title}</h3>
 		<IconDots variant="heading-three" />
 	</div>
 
-	<p>{description}</p>
+	<p class="info-card__description">{description}</p>
 
 	{#if link}
-		<div class="link">
+		<div class="info-card__link">
 			<RLink
 				href={link.href}
 				class="button-outline-blue clickable-container"
 			>
 				{link.label}
 				<span
-					class="info-card-cta-arrow"
+					class="info-card__cta-arrow"
 					aria-hidden="true">→</span
 				>
 			</RLink>
@@ -33,43 +33,35 @@
 		background: light-dark(var(--background), var(--blue-800));
 		border-radius: 20px;
 		padding: 2rem;
-		display: flex;
-		flex-direction: column;
-		gap: 0.75rem;
-		/* position: relative; */
-
-		h3 {
-			grid-row: 1;
-			grid-column: 2;
-		}
-
-		.heading {
-			display: flex;
-			flex-direction: row-reverse;
-			gap: 0.75rem;
-			align-self: start;
-			align-items: center;
-		}
-
-		p {
-			grid-row: 2;
-			grid-column: 1/-1;
-			margin-bottom: 1rem;
-		}
-
-		.link {
-			grid-row: 3;
-			grid-column: 1/-1;
-		}
-	}
-
-	article {
-		border: 1px solid red;
-
 		display: grid;
 		grid-template-columns: 1fr;
 		gap: 0.75rem;
 		width: 100%;
 		max-width: 55ch;
+		position: relative;
+
+		.info-card__heading {
+			display: flex;
+			flex-direction: row-reverse;
+			gap: 0.75rem;
+			justify-self: start;
+			align-items: center;
+		}
+
+		.info-card__title {
+			grid-row: 1;
+			grid-column: 2;
+		}
+
+		.info-card__description {
+			grid-row: 2;
+			grid-column: 1/-1;
+			margin-bottom: 1rem;
+		}
+
+		.info-card__link {
+			grid-row: 3;
+			grid-column: 1/-1;
+		}
 	}
 </style>
