f7c51fc77e7c8b7e50bf6ecb9f35b216df916628
vsheo <vireshsheoratan@gmail.com>
2026-05-28 11:03:19 +0200

    feat: filter buttons component #643

---
+++ src/lib/refactored/molecules/FilterButtons.svelte
@@ -0,0 +1,54 @@
+<script>
+	const { categories, selectedCategory } = $props()
+</script>
+
+<div class="filter-buttons">
+	<p>Filter op categorie:</p>
+	<div class="filter">
+		<a
+			href="?category=alle-publicaties"
+			class="button-outline-blue {selectedCategory === 'alle-publicaties' ? 'active' : ''}"
+			data-sveltekit-noscroll
+		>
+			Alle publicaties</a
+		>
+
+		{#each categories as categorie (categorie.id)}
+			<a
+				href={`?category=${categorie.title.toLowerCase()}`}
+				data-sveltekit-noscroll
+				class="button-outline-blue {selectedCategory.toLowerCase() === categorie.title.toLowerCase() ? 'active' : ''}"
+				>{categorie.title}
+			</a>
+		{/each}
+	</div>
+</div>
+
+<style>
+	.filter-buttons {
+		display: flex;
+		flex-direction: column;
+		flex-wrap: wrap;
+		gap: 1em;
+		width: 90%;
+		padding: 3em 0 0 0;
+		margin: auto;
+
+		@media (min-width: 768px) {
+			padding: 5em 0 0 0;
+			max-width: 1400px;
+		}
+
+		.filter {
+			display: flex;
+			flex-wrap: wrap;
+			flex-direction: row;
+			gap: 1em;
+		}
+	}
+
+    	.button-outline-blue.active {
+		background-color: var(--primary-blue);
+		color: white;
+	}
+</style>
--- src/routes/(public)/publicaties/+page.svelte
+++ src/routes/(public)/publicaties/+page.svelte
@@ -7,7 +7,7 @@
 	import { publicatie } from '$lib'
 
 	// Import components
-	import { Hero } from '$lib'
+	import { Hero, RFilterButtons } from '$lib'
 
 	// Haal data op uit page.server.js via props
 	const { data } = $props()
@@ -49,27 +49,7 @@
 	/>
 </Hero>
 
-<div class="filter-buttons">
-	<p>Filter op categorie:</p>
-	<div class="filter">
-		<a
-			href="?category=alle-publicaties"
-			class="button-outline-blue {selectedCategory === 'alle-publicaties' ? 'active' : ''}"
-			data-sveltekit-noscroll
-		>
-			Alle publicaties</a
-		>
-
-		{#each categories as categorie (categorie.id)}
-			<a
-				href={`?category=${categorie.title.toLowerCase()}`}
-				data-sveltekit-noscroll
-				class="button-outline-blue {selectedCategory.toLowerCase() === categorie.title.toLowerCase() ? 'active' : ''}">{categorie.title}
-				
-			</a>
-		{/each}
-	</div>
-</div>
+<RFilterButtons {categories} {selectedCategory}/>
 
 <div class="section-documents">
 	<div class="filter-info">
@@ -86,33 +66,6 @@
 </div>
 
 <style>
-	.filter-buttons {
-		display: flex;
-		flex-direction: column;
-		flex-wrap: wrap;
-		gap: 1em;
-		width: 90%;
-		padding: 3em 0 0 0;
-		margin: auto;
-
-		@media (min-width: 768px) {
-			padding: 5em 0 0 0;
-			max-width: 1400px;
-		}
-
-		.filter {
-			display: flex;
-			flex-wrap: wrap;
-			flex-direction: row;
-			gap: 1em;
-		}
-	}
-
-	.button-outline-blue.active {
-		background-color: var(--primary-blue);
-		color: white;
-	}
-
 	.section-documents {
 		display: flex;
 		flex-direction: column;
