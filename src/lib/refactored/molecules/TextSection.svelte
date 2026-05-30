3080140213c533ebd2e3de8a091a7eef040d8828
Sidopjescherm <hermussidney@gmail.com>
2026-05-30 12:41:38 +0200

    refactor: #653 Added script and HTML
    For the use of this section I load in the Directus, Refactored link atom and the sourceFileID. I'm testing on the slug, page right now, but this will be combined with the hero and ' Kom naar de Ad-dag'  Link atom. I will remove the button once I start on the organism.

---
+++ src/lib/refactored/molecules/TextSection.svelte
@@ -0,0 +1,50 @@
+<script>
+    import { DIRECTUS_URL } from '$lib/constants.js'
+    import { RLink } from '$lib';
+
+    export let data
+
+    const sourceFileId = data?.document?.source_file?.id ?? data?.document?.source_file ?? null
+</script>
+
+<svelte:head>
+	<title>Documenten | Overlegplatform Associate Degrees</title>
+</svelte:head>
+
+<div class="wrapper-detail">
+	<article class="detail">
+		<p>{data.document.description}</p>
+		{#if sourceFileId}
+			<div class="file">
+				<p>
+					Hieronder een preview van het document of bekijk 
+                    <RLink
+                        target="_blank"
+                        href={`${DIRECTUS_URL}/assets/${sourceFileId}`}
+                    >
+                        hier
+                    </RLink>
+                    het hele document
+				</p>
+				<iframe
+					title={data.document.title}
+					src={`${DIRECTUS_URL}/assets/${sourceFileId}`}
+				></iframe>
+			</div>
+		{/if}
+        </article>
+<!-- 
+	<div class="wrapper-ad-day">
+		<section class="ad-day">
+			<h2>Kom naar de Ad-dag!</h2>
+			<p>Ontdek alles over Associate Degrees en laat je inspireren tijdens workshops en presentaties op de Landelijke Ad-dag.</p>
+            <RLink
+                href="/ad-dag"
+                class="button-outline-white"
+            >
+                Meer weten over de AD-dag
+            </RLink>
+		</section>
+	</div> -->
+</div>
+
