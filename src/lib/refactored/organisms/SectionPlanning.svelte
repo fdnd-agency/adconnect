247f58a0552eef43e1de25d87503537c65f41ea9
vsheo <vireshsheoratan@gmail.com>
2026-06-01 12:51:37 +0200

    feat: styled planning section #668

--- src/lib/refactored/organisms/SectionPlanning.svelte
+++ src/lib/refactored/organisms/SectionPlanning.svelte
@@ -5,24 +5,23 @@
 	const { sectionInfo, cardsData } = $props()
 </script>
 
-<section>
+<section class="card-section">
 	<RCardSection
 		title={sectionInfo.title}
 		description={sectionInfo.description}
 	/>
 
-	<section class="cards">
+	<div class="card-section__list">
+
 		{#each cardsData as card (card.id)}
 			<article class="card">
-				<div class="wrapper-text">
+				<div class="card__header">
 					<IconDots variant="heading-three" />
-					<h3>{card.title}</h3>
+					<h3 class="card__title">{card.title}</h3>
 				</div>
 
 				{#if card.description}
-					<p>
-						{card.description}
-					</p>
+					<p class="card__text">{card.description}</p>
 				{:else if card.schedule}
 					<RTimeTable scheduleData={card.schedule} />
 				{/if}
@@ -30,82 +29,66 @@
 				<RLink
 					class="button-outline-blue"
 					href={card.link.href}
-					>{card.link.label}
+				>
+					{card.link.label}
 				</RLink>
 			</article>
 		{/each}
-	</section>
+
+	</div>
 </section>
 
 <style>
-	.cards {
+	.card-section {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin: 5em min(5%, 3em);
+	}
+
+	.card-section__list {
 		display: grid;
 		grid-template-columns: 1fr;
-		grid-template-rows: repeat(4, auto);
-		margin-top: 5em;
-		gap: 1em;
-		margin-bottom: 2em;
+		gap: 5em;
+		margin: 5em 0;
 
-		article {
-			border: 1px solid #cccccc;
-			border-radius: 1em;
-			padding: 2em;
-			transition: 0.2s ease-in-out;
-			display: flex;
-			flex-direction: column;
-			gap: 1em;
-			background-color: light-dark(var(--text-white), hsl(210, 30%, 8%));
-			text-align: center;
-			margin: 0 auto;
-
-			p {
-				max-width: 500px;
-			}
-
-			&:hover {
-				border: 1px solid #00408d;
-				box-shadow: 0 3px 10px rgba(141, 141, 141, 0.2);
-				translate: 0 -1%;
-				transition: 0.2s ease-in-out;
-			}
-		}
-
-		h2 {
-			color: var(--primary-blue);
+		@media (min-width: 1024px) {
+			grid-template-columns: repeat(2, 1fr);
+			gap: 5em;
 		}
 	}
 
 	.card {
-		display: grid;
-		grid-template-rows: subgrid;
-		grid-row: span 4;
-		border: 1px solid var(--text-white);
-		background-color: var(--text-white);
-		color: light-dark(var(--primary-blue), var(--text-white));
-		border-radius: 24px;
+		--_border-color: #cccccc;
+
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 1em;
 		padding: 2em;
+		border: 1px solid var(--_border-color);
+		border-radius: 1em;
+		background-color: light-dark(var(--text-white), hsl(210, 30%, 8%));
+		color: light-dark(var(--primary-blue), var(--text-white));
+		max-width: 600px;
 
-		.logo {
-			margin: 0;
-			text-align: left;
-			float: left;
-		}
-		h2 {
-			margin-top: 0;
-			margin-bottom: 0.5em;
-			right: 1em;
-			position: relative;
+		transition: 0.2s ease-in-out;
+
+		&:hover {
+			--_border-color: #00408d;
+			box-shadow: 0 3px 10px rgba(141, 141, 141, 0.2);
+			translate: 0 -1%;
 		}
 	}
 
-	a {
-		margin-top: 1em;
-		align-self: center;
+	.card__header {
+		display: flex;
+		gap: 2em;
+		align-self: start;
 	}
-	@media (min-width: 1024px) {
-		.cards {
-			grid-template-columns: repeat(2, 1fr);
-			gap: 0.5em;
-		}
+
+	.card__text {
+		max-width: 500px;
+		margin-bottom: auto;
 	}
 </style>
