a76e529fd060ec0811371a0b3b3509a8ba7d770d
Alex <alex.louwe@hva.nl>
2026-06-01 12:40:38 +0200

    feat: improves the NominationsCarousel adds institution logo to beneath the names adds links to the nomination detailpages issue #671 userstory #649

--- src/routes/(public)/talent-award/+page.server.js
+++ src/routes/(public)/talent-award/+page.server.js
@@ -2,14 +2,18 @@ import { error } from '@sveltejs/kit'
 import { ContentService } from '$lib/server/contentService.js'
 
 export async function load() {
+	const cooperationFields = 'id,name,logo'
 	const nominationsResponse = await ContentService.fetchContent('nominations', null, null, null, false)
+	const cooperationsResponse = await ContentService.fetchContent('cooperations', null, cooperationFields, null, false)
 	const nominations = Array.from(nominationsResponse.data.nominations?.values?.() ?? [])
+	const cooperations = Array.from(cooperationsResponse.data.cooperations?.values?.() ?? [])
 
 	if (nominations.length === 0) {
 		throw error(404, 'Talent en Nominaties niet gevonden')
 	}
 
 	return {
-		nominations
+		nominations,
+		cooperations
 	}
 }
