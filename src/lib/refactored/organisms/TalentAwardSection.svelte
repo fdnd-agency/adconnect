2cad59131f18df84b486eefff1be747e86ec9ce9
Sidopjescherm <hermussidney@gmail.com>
2026-06-01 15:33:59 +0200

    refactor: #608 Created talentaward section
    I created the talent award organism so I can later use other molecules to make the page

---
+++ src/lib/refactored/organisms/TalentAwardSection.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { RTalentWinner } from "$lib"
+    const { data } = $props()
+</script>
+
+<section>
+    <RTalentWinner { data }/>
+</section>
+
+<style>
+    section {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 1.5rem;
+		margin-bottom: 2rem;
+		margin-top: 1rem;
+	}
+</style>
