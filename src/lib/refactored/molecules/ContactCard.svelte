c9c3bb8f6d233a2a163fb8fe4a4814223e844700
Sidopjescherm <hermussidney@gmail.com>
2026-06-02 10:29:31 +0200

    refactor: #684  Added Cardsection and address
    I added the card section to the component and started adding links around the address. It doesn't seem to work right now so I will contact @vsheo about this. I think it might have something to do with the Card component not showing anything else.

---
+++ src/lib/refactored/molecules/ContactCard.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { RLink, RCardSection } from "$lib";
+</script>
+
+<RCardSection
+    title="Contact gegevens"
+    description=""
+>
+
+</RCardSection>
+
+    <address>
+        <RLink
+            href="tel:+0205995555"
+        >
+            Telefoonnummer
+        </RLink>
+    
+        <RLink
+            href="mailto:platformads@outlook.com"
+        >
+            platformads@outlook.com
+        </RLink>
+    </address>
