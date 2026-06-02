2d31dccc813f43794e826950fc2b54537032c731
Sidopjescherm <hermussidney@gmail.com>
2026-06-02 11:57:38 +0200

    refactor: #684 added static elements
    This component will not YET be used anywhere on the site, however I am making it so it can later be used for other pages if need be.

--- src/lib/refactored/molecules/ContactCard.svelte
+++ src/lib/refactored/molecules/ContactCard.svelte
@@ -1,24 +1,55 @@
 <script>
-    import { RLink, RCardSection } from "$lib";
+    import { RLink, RCardSection, label, mail, phone, RPicture } from "$lib"
 </script>
 
-<RCardSection
-    title="Contact gegevens"
-    description=""
->
+<section>
+    <h2>
+        Contactgegevens
+    </h2>
 
-</RCardSection>
+    <p>
+        Heb je vragen of wil je meer weten over Associate Degrees neem dan via het onderstaande formulier contact met ons op.
+    </p>
 
     <address>
         <RLink
             href="tel:+0205995555"
+            class="nav-link"
         >
-            Telefoonnummer
+            +0205995555
         </RLink>
     
         <RLink
             href="mailto:platformads@outlook.com"
+            class="nav-link"
         >
             platformads@outlook.com
         </RLink>
     </address>
+</section>
+
+<style>
+    section {
+		background-color: light-dark(var(--text-white), var(--primary-blue));
+		border: 1px solid var(--neutral-300);
+		border-radius: 1em;
+		padding: 1.5em;
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+        gap: 1em;
+
+        p {
+            width: clamp(25ch, auto, 40)
+        }
+    }
+
+    address {
+        display: flex;
+        flex-direction: column;
+        font-style: normal;
+        gap: 1em;
+
+        padding: 2em 0 0 0;
+    }
+</style>
