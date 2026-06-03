e6ae5317d29de9e04ce96250a1c3c8b474bac417
geemenw <wesley.van.geemen@hva.nl>
2026-06-03 12:42:02 +0200

    feat:#713-Contentservice update

--- src/lib/server/contentService.test.js
+++ src/lib/server/contentService.test.js
@@ -74,7 +74,8 @@ describe('ContentService property-based tests', () => {
 			'sectoralAdvisoryBoards',
 			'pageHome',
 			'pageAboutAd',
-			'pageLado'
+			'pageLado',
+			'pageAboutUs'
 		])
 
 		await fc.assert(
--- src/lib/server/strategies/directusContentStrategy.js
+++ src/lib/server/strategies/directusContentStrategy.js
@@ -20,7 +20,8 @@ const COLLECTIONS = {
 	sectoralAdvisoryBoards: { path: 'adconnect_sectoral_advisory_boards', key: 'id' },
 	pageHome: { path: 'adconnect_page_home', key: 'id' },
 	pageAboutAd: { path: 'adconnect_page_about_ad', key: 'id' },
-	pageLado: { path: 'adconnect_page_lado', key: 'id' }
+	pageLado: { path: 'adconnect_page_lado', key: 'id' },
+	pageAboutUs: { path: 'adconnect_page_about_us', key: 'id' }
 }
 
 function getConfig(contentType) {
