294e046992cc0ea74402f46240941318a2b13de2
Sidopjescherm <hermussidney@gmail.com>
2026-06-03 12:16:25 +0200

    refactor: #684 Circle and text fix

--- src/lib/refactored/molecules/ContactCard.svelte
+++ src/lib/refactored/molecules/ContactCard.svelte
@@ -1,9 +1,9 @@
 <script>
-    import { RLink, RCardSection, label, mail, phone, RPicture } from "$lib"
+    import { RLink, RCardSection, lightcircle, mail, phone, RPicture } from "$lib"
     const { ...props }=$props()
 </script>
 
-<section>
+<section class="contact-card">
     <h2>
         Contactgegevens
     </h2>
@@ -12,7 +12,7 @@
         Heb je vragen of wil je meer weten over Associate Degrees neem dan via het onderstaande formulier contact met ons op.
     </p>
 
-    <address>
+    <address class="contact-card__details">
         <RLink
             href="tel:+0205995555"
             class="nav-link"
@@ -20,7 +20,7 @@
             <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M16.4062 1.78125C16.75 1.875 17 2.15625 17 2.5C17 10.5312 10.5 17 2.5 17C2.125 17 1.84375 16.7812 1.75 16.4375L1 13.1875C0.9375 12.8438 1.09375 12.4688 1.4375 12.3125L4.9375 10.8125C5.25 10.6875 5.59375 10.7812 5.8125 11.0312L7.375 12.9375C9.8125 11.7812 11.7812 9.78125 12.9062 7.40625L11 5.84375C10.75 5.625 10.6562 5.28125 10.7812 4.96875L12.2812 1.46875C12.4375 1.125 12.8125 0.9375 13.1562 1.03125L16.4062 1.78125Z" fill="white"/>
             </svg>
-            +0205995555
+            Telefoonnummer
         </RLink>
         
         <RLink
@@ -33,26 +33,51 @@
             platformads@outlook.com
         </RLink>
     </address>
+
+    <svg class="circle-info" width="469" height="469" viewBox="0 0 469 469" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M437.388 117.684C502.016 229.624 463.663 372.76 351.724 437.388C239.784 502.016 96.648 463.663 32.0198 351.724C-32.6085 239.784 5.74481 96.648 117.684 32.0198C229.624 -32.6084 372.76 5.74482 437.388 117.684Z" fill="#97AED5" fill-opacity="0.15"/>
+    </svg>
+
 </section>
 
 <style>
-    section {
+    .contact-card {
 		background-color: light-dark(var(--text-white), var(--primary-blue));
 		border: 1px solid var(--neutral-300);
 		border-radius: 1em;
+
 		padding: 1.5em;
+
 		display: flex;
 		flex-direction: column;
 		align-items: flex-start;
-        gap: 1em;
+        gap: 2em;
+        position: relative;
+
+        overflow: hidden;
+
+        @media (min-width: 1080px) {
+            width: 40%;        
+        }
     }
 
-    address {
+    .contact-card__details {
         display: flex;
         flex-direction: column;
         font-style: normal;
         gap: 1em;
+    }
 
-        padding: 2em 0 0 0;
+    .circle-info {
+		display: none;
+
+		@media (min-width: 875px) {
+			width: 17em;
+            right: -10%;
+            bottom: -40%;
+			display: block;
+			position: absolute;
+            
+		}
     }
 </style>
