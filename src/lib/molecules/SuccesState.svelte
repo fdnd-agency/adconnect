8b636d547a579de387efcc4c33e6fb86cad36465
Viresh Sheoratan <vireshsheoratan@gmail.com>
2026-06-05 09:27:10 +0200

    fix: missing success image #719

--- src/lib/molecules/SuccesState.svelte
+++ src/lib/molecules/SuccesState.svelte
@@ -1,38 +1,55 @@
 <script>
-    import { birdcheck } from '$lib'
-    export let status
+	import { RPicture, birdcheck } from '$lib'
+	export let status
 </script>
 
 {#if status === 'success'}
-    <section class="success visible">
-        <div class="info">
-            <h3>Bedankt, je formulier is verzonden!</h3>
-            <p>Jouw formulier is succesvol naar ons verzonden. We nemen zo snel mogelijk contact op per mail.</p>
-        </div>
-        <img src="{birdcheck}" alt="">
-    </section>
+	<section class="success visible">
+		<div class="info">
+			<h3>Bedankt, je formulier is verzonden!</h3>
+			<p>Jouw formulier is succesvol naar ons verzonden. We nemen zo snel mogelijk contact op per mail.</p>
+		</div>
+		<div class="img-container">
+			<RPicture
+				isEnhanced
+				src={birdcheck}
+				alt=""
+				width="300px"
+				height="300px"
+				fetchpriority="high"
+				loading="eager"
+				style="height:auto;"
+			/>
+		</div>
+	</section>
 {/if}
 
 <style>
-    .success {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
+	.success {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+        gap: 1em;
 
-        .info {
-            width: 100%;
-            display: flex;
-            flex-direction: column;
-            gap: 1em;
+		.info {
+			width: 100%;
+			display: flex;
+			flex-direction: column;
+			gap: 1em;
 
-            @media (min-width: 768px) {
-                width: 50%;
-            }
+			@media (min-width: 768px) {
+				width: 50%;
+			}
+		}
+
+        .img-container {
+            max-height: 300px;
+            max-width: 300px;
         }
 
-        @media (min-width: 768px) {
-            flex-direction: row;
-        }
-    }
+		@media (min-width: 768px) {
+			flex-direction: row;
+		}
+	}
 </style>
